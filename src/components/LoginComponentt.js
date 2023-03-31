d4188c2281863e25db39c5fb45d9e4563ffb8241
Apollo0102 <crazyprince0012@gmail.com>
2023-03-31 16:06:44 -0500

    update some bugs

--- src/components/LoginComponentt.js
+++ src/components/LoginComponentt.js
@@ -10,7 +10,7 @@ class LoginComponent extends React.Component {
 
     handleOnChange = (e) => {
         e.persist();
-        console.log(e.target.value);
+        console.log(e.target.valu, "11111111111");
         this.setState(() => ({
             [e.target.name]: e.target.value 
         }))
